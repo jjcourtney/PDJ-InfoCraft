a05e18b00b6472f8eebf0fcfd3fed5bec546a830
dylson <dylsonuni@gmail.com>
2021-07-17 14:20:15 +0100

    created dynamic update feature w/Jquery

--- assets/js/hypixelApi.js
+++ assets/js/hypixelApi.js
@@ -1,21 +1,19 @@
 
+ const UUID = "ad1fe1440d1241428e9d646cb29f0a0e"
+ const API_KEY ="5f3485f6-646a-4355-bf67-74272a6c9efb"
 
-function getPlayerStats() {
-
-    const UUID = "ad1fe1440d1241428e9d646cb29f0a0e"
-    const API_KEY ="5f3485f6-646a-4355-bf67-74272a6c9efb"
-
-
-    return fetch(`https://api.hypixel.net/player?uuid=${UUID}&key=${API_KEY}`)
+ const getPlayerStats = () => fetch(`https://api.hypixel.net/player?uuid=${UUID}&key=${API_KEY}`)
     .then(response => response.json())
-    .then(data => ({
+    .then(data => 
+        ({
         playerUUID: data.player.uuid,
         playerName: data.player.playername,
         firstLogin: data.player.firstLogin,
         lastLogin: data.player.lastLogin,
         achievementPoints: data.player.achievementPoints,
         numAchievements: data.player.achievementsOneTime.length
-    }))
+        })
+    );
 
-}
+
 getPlayerStats().then(playerObj => console.log(playerObj));
--- assets/js/serverStatus.js
+++ assets/js/serverStatus.js
@@ -3,12 +3,14 @@ const address ="hypixel.net";
 
 const getHypixelStatus = () => fetch(`https://api.mcsrvstat.us/2/${address}`)
     .then(response => response.json())
-    .then(data => ({
+    .then(data => 
+        ({
         isServerOnline : data.online,
         maxPlayers : data.players.max,
         onlinePlayers : data.players.online,
         hostname: data.hostname
-    }));
+        })
+    );
 
 
 function updateStatus(statusObj){
@@ -16,7 +18,6 @@ function updateStatus(statusObj){
 }
 
 getHypixelStatus().then((data) => {
-        debugger;
      updateStatus(data)
     })
 
