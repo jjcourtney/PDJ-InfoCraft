0b12105e433bdc2377531343d8de498103dbe301
jjcourtney <jcourtneywork@gmail.com>
2021-07-23 13:03:56 +0100

    removed excess w/s - added comments - refactor

--- assets/js/hypixelApi.js
+++ assets/js/hypixelApi.js
@@ -1,5 +1,5 @@
 
- // const UUID = "ad1fe1440d1241428e9d646cb29f0a0e"
+ 
  const API_KEY ="5f3485f6-646a-4355-bf67-74272a6c9efb"
 
  const getPlayerStats = uuid => fetch(`https://api.hypixel.net/player?uuid=${uuid}&key=${API_KEY}`)
@@ -27,11 +27,5 @@
             numAchievements: "N/A",
             foundPlayer: data.success
         }
-        
+    });
 
-        
-    }
-    );
-
-
-getPlayerStats().then(playerObj => console.log(playerObj));
